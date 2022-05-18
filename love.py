16efe8b54fc49831e959062cb932e10976ada2fd
ujjaltech <53267860+ujjaltech@users.noreply.github.com>
2022-05-18 23:04:11 +0530

    Add files via upload

---
+++ love.py
@@ -0,0 +1,16 @@
+from turtle import *
+bgcolor("light green")
+def lovemove():
+    for i in range(200):
+        right(1)
+        forward(1)
+color('black','red')       
+begin_fill()
+left(140)
+forward(111.65)
+lovemove()
+left(120)
+lovemove()
+forward(111.65)
+end_fill()
+done()
